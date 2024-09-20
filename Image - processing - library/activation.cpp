68a97d5f244270c4f7359f094606f807cd5b7f5a
yeatrix <lakshyakumar.iitd@gmail.com>
2024-09-20 12:26:38 +0530

    added the activation module

---
+++ Image - processing - library/activation.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+float ReLu(float x){
+    /*
+        ReLu activation function 
+    */
+    return x > 0 ? x : 0;
+}
+
+float tanh(float x){
+    /*
+        tanh activation function
+    */
+    float activated_x = (exp(x)-exp(-x))/(exp(x)-exp(-x));
+    return activated_x;
+}
+
+void apply_relu(vector<vector<float> > &matrix){
+    /*
+        function to apply the relu activation function on the input matrix
+    */
+    int m = matrix.size();
+    int n = matrix[0].size();
+
+    for (int i = 0; i < m; i++){
+        for (int j = 0; j < n; j++){
+            matrix[i][j] = ReLu(matrix[i][j]);
+        }
+    }
+}
+
+void apply_relu(vector<vector<float> > &matrix){
+    /*
+        function to apply the tanh activation function on the input matrix
+    */
+    int m = matrix.size();
+    int n = matrix[0].size();
+
+    for (int i = 0; i < m; i++){
+        for (int j = 0; j < n; j++){
+            matrix[i][j] = tanh(matrix[i][j]);
+        }
+    }
+}
+
+int main(){
+
+}
