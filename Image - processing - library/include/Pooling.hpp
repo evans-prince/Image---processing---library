482b17ec725afc70b19541348e8ce840c8eed660
Raunit123 <tt1222160@textile.iitd.ac.in>
2024-09-21 13:15:31 +0530

    updated the pooling header file

--- Image - processing - library/include/Pooling.hpp
+++ Image - processing - library/include/Pooling.hpp
@@ -5,9 +5,17 @@
 //  Created by RAUNIT SINGH on 20/09/24.
 //
 
-#ifndef Pooling_hpp
-#define Pooling_hpp
+#ifndef POOLING_HPP
+#define POOLING_HPP
 
-#include <stdio.h>
+#include <vector>
 
-#endif /* Pooling_hpp */
+using namespace std;
+
+// Function declaration for Max Pooling
+vector<vector<int>> MaxPooling(vector<vector<int>> &matrix, int poolsize, int stride);
+
+// Function declaration for Average Pooling
+vector<vector<int>> AvgPooling(vector<vector<int>> &matrix, int poolsize, int stride);
+
+#endif /* POOLING_HPP */
