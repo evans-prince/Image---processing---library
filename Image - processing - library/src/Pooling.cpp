15c01724278bb8a4504f855e396dbb076e90c978
Raunit123 <tt1222160@textile.iitd.ac.in>
2024-09-21 12:29:03 +0530

    defined function for pooling

---
+++ Image - processing - library/src/Pooling.cpp
@@ -0,0 +1,8 @@
+//
+//  Pooling.cpp
+//  Image - processing - library
+//
+//  Created by RAUNIT SINGH on 20/09/24.
+//
+
+#include "Pooling.hpp"
---
+++ Image - processing - library/src/Pooling.hpp
@@ -0,0 +1,13 @@
+//
+//  Pooling.hpp
+//  Image - processing - library
+//
+//  Created by RAUNIT SINGH on 20/09/24.
+//
+
+#ifndef Pooling_hpp
+#define Pooling_hpp
+
+#include <stdio.h>
+
+#endif /* Pooling_hpp */
