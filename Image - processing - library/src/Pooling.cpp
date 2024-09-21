73cf7e79b1cc8fb4f1472a595e44db1f665b866d
Raunit123 <117707436+Raunit123@users.noreply.github.com>
2024-09-21 12:39:53 +0530

    Update Pooling.cpp

--- Image - processing - library/src/Pooling.cpp
+++ Image - processing - library/src/Pooling.cpp
@@ -4,5 +4,60 @@
 //
 //  Created by RAUNIT SINGH on 20/09/24.
 //
-
+#include<iostream>
+#include<vector>
 #include "Pooling.hpp"
+
+using namespace std;
+
+vector<vector<int>> MaxPooling(vector<vector<int>> &matrix,int poolsize,int stride){
+    int h=matrix.size();
+    int w=matrix[0].size();
+    
+    int output_h=(h-poolsize)/stride + 1;
+    int output_w=(h-poolsize)/stride + 1;
+    
+    
+    vector<vector<int>> pooledOutput(output_h,vector<int>(output_w,0));
+    
+    for(int i=0;i<=h-poolsize;i+=stride){
+        for(int j=0;j<=w-poolsize;j+=stride){
+            int maxVal=matrix[i][j];
+            for(int x=0;x<poolsize;++x){
+                for(int y=0;y<poolsize;++y){
+                    maxVal=max(matrix[i+x][j+y],maxVal);
+                }
+            }
+            pooledOutput[i/stride][j/stride]=maxVal;
+        }
+    }
+    return pooledOutput;
+    
+};
+
+
+vector<vector<int>> AvgPooling(vector<vector<int>> &matrix,int poolsize,int stride){
+    int h=matrix.size();
+    int w=matrix[0].size();
+    
+    int output_h=(h-poolsize)/stride + 1;
+    int output_w=(h-poolsize)/stride + 1;
+    
+    
+    vector<vector<int>> pooledOutput(output_h,vector<int>(output_w,0));
+    
+    for(int i=0;i<=h-poolsize;i+=stride){
+        for(int j=0;j<=w-poolsize;j+=stride){
+            int sum=0;
+            for(int x=0;x<poolsize;++x){
+                for(int y=0;y<poolsize;++y){
+                    sum=sum+matrix[i+x][j+y];
+                }
+            }
+            pooledOutput[i/stride][j/stride]=sum/(poolsize*poolsize);
+        }
+    }
+    return pooledOutput;
+    
+};
+
